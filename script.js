db8598ce805f268b2910876c71d019a4eb2ea9c7
BarryCap <barthelemy.perez@gmail.com>
2021-06-07 22:41:39 +0200

    Add shortcuts to all 58 pages Add script.js with keyboard shortcuts Add script.js link to all 58 pages

---
+++ script.js
@@ -0,0 +1,41 @@
+window.addEventListener('keyup', function(event) {
+  if (event.code == 'KeyB') {
+    window.location = "/"
+  }
+  if (event.code == 'KeyR') {
+    window.location = "/realizations"
+  }
+  if (event.code == 'KeyA') {
+    window.location = "/about"
+  }
+  if (event.code == 'KeyN') {
+    window.location = "/random"
+  }
+  if (event.code == 'KeyI') {
+    window.location = "/realizations/images"
+  }
+  if (event.code == 'KeyV') {
+    window.location = "/realizations/videos"
+  }
+  if (event.code == 'KeyS') {
+    window.location = "/realizations/sounds"
+  }
+  if (event.code == 'KeyG') {
+    window.location = "/realizations/games"
+  }
+  if (event.code == 'Digit7') {
+    window.location = "/realizations/Imaractères"
+  }
+  if (event.code == 'KeyL') {
+    window.location = "/spotlight"
+  }
+  if (event.code == 'F22') {
+    window.location = "/404"
+  }
+  if (event.code == 'KeyH') {
+    window.location = "/help"
+  }
+  if (event.code == 'KeyE') {
+    window.location = "/versions"
+  }
+}, true)
