3b1f5a4fc93dde8c5e5e1688e9a0740ab99673a9
BarryCap <barthelemy.perez@gmail.com>
2021-06-08 11:57:41 +0200

    Prevent from using shortcuts in `form` Fix #65

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
 // Shortcuts
 window.addEventListener('keyup', function(event) {
-  if (event.code == 'KeyB') {
-    window.location = "/"
-  }
-  if (event.code == 'KeyR') {
-    window.location = "/realizations"
-  }
-  if (event.code == 'KeyA') {
-    window.location = "/about"
-  }
-  if (event.code == 'KeyN') {
-    window.location = "/random"
-  }
-  if (event.code == 'KeyI') {
-    window.location = "/realizations/images"
-  }
-  if (event.code == 'KeyV') {
-    window.location = "/realizations/videos"
-  }
-  if (event.code == 'KeyS') {
-    window.location = "/realizations/sounds"
-  }
-  if (event.code == 'KeyG') {
-    window.location = "/realizations/games"
-  }
-  if (event.code == 'Digit7') {
-    window.location = "/realizations/Imaractères"
-  }
-  if (event.code == 'KeyL') {
-    window.location = "/spotlight"
-  }
-  if (event.code == 'F22') {
-    window.location = "/404"
-  }
-  if (event.code == 'KeyH') {
-    window.location = "/help"
-  }
-  if (event.code == 'KeyE') {
-    window.location = "/versions"
+  if(! event.path.filter((node) => {return node.nodeName == 'FORM'}).length) {
+    if (event.code == 'KeyB') {
+      window.location = "/"
+    }
+    if (event.code == 'KeyR') {
+      window.location = "/realizations"
+    }
+    if (event.code == 'KeyA') {
+      window.location = "/about"
+    }
+    if (event.code == 'KeyN') {
+      window.location = "/random"
+    }
+    if (event.code == 'KeyI') {
+      window.location = "/realizations/images"
+    }
+    if (event.code == 'KeyV') {
+      window.location = "/realizations/videos"
+    }
+    if (event.code == 'KeyS') {
+      window.location = "/realizations/sounds"
+    }
+    if (event.code == 'KeyG') {
+      window.location = "/realizations/games"
+    }
+    if (event.code == 'Digit7') {
+      window.location = "/realizations/Imaractères"
+    }
+    if (event.code == 'KeyL') {
+      window.location = "/spotlight"
+    }
+    if (event.code == 'F22') {
+      window.location = "/404"
+    }
+    if (event.code == 'KeyH') {
+      window.location = "/help"
+    }
+    if (event.code == 'KeyE') {
+      window.location = "/versions"
+    }
   }
 })
 // Randomly moving background
