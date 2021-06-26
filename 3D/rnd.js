ddb0eee26e3d0d1dcf4a391ef1e526ea02d459ff
BarryCap <barthelemy.perez@gmail.com>
2021-06-26 12:00:12 +0200

    Make Random Object 3D page work Set color filter of Random Object 3D page randomly

--- 3D/rnd.js
+++ 3D/rnd.js
@@ -1,24 +1,21 @@
+const hue = (Math.floor(Math.random() * 360)) + 'deg'
+document.documentElement.style.setProperty('--rnd-hue', hue)
 function randomObj() {
   const vNum = 128
   const fNum = 128
-  document.open()
+  let OBJ = ''
   for (let i = 0; i <= vNum; i++) {
     const num1 = Math.round((Math.random() * 1024) - 512)
     const num2 = Math.round((Math.random() * 1024) - 512)
     const num3 = Math.round((Math.random() * 1024) - 512)
-    document.writeln('v ' + num1 + ' ' + num2 + ' ' + num3)
+    OBJ += 'v ' + num1 + ' ' + num2 + ' ' + num3 + '\n'
   }
   for (let i = 0; i <= fNum; i++) {
     const num1 = Math.floor((Math.random() * vNum) + 1)
     const num2 = Math.floor((Math.random() * vNum) + 1)
     const num3 = Math.floor((Math.random() * vNum) + 1)
-    const num4 = Math.floor((Math.random() * vNum) + 1)
-    document.writeln('f ' + num1 + ' ' + num2 + ' ' + num3 + ' ' + num4)
+    OBJ += 'f ' + num1 + ' ' + num2 + ' ' + num3 + '\n'
   }
-  /* Random color
-  let hue
-  hue = (Math.floor(Math.random() * 360)) + 'deg'
-  document.documentElement.style.setProperty('--random-hue')
-  */
-  document.close()
+  const file = new File([OBJ], "rnd.obj", {type: "text/plain"})
+  return window.URL.createObjectURL(file)
 }
