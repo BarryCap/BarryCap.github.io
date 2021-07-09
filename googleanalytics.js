650a55f876cad27d18c110b634341dc3a11f5658
BarryCap <barthelemy.perez@gmail.com>
2021-07-09 19:45:38 +0200

    Clean `googleanalytics.js`

--- googleanalytics.js
+++ googleanalytics.js
@@ -1,5 +1,6 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-GY5Q2KLJ19');
+window.dataLayer = window.dataLayer || []
+function gtag() {
+  dataLayer.push(arguments)
+}
+gtag('js', new Date())
+gtag('config', 'G-GY5Q2KLJ19')
