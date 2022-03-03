1263e08fceadecb5c9b71b63484dbdad4dc893b6
BarryCap <barthelemy.perez@gmail.com>
2022-03-03 22:14:53 +0100

    Update 25 folder

--- sounds/25/squares.js
+++ sounds/25/squares.js
@@ -1,4 +1,6 @@
 let mouseDown = false
+let randomButton = [false, false]
+let randomPlay = {}
 
 async function key(url, press) {
   if (mouseDown || press) {
@@ -15,4 +17,22 @@ document.firstChild.onmousedown = () => {
 
 document.firstChild.onmouseup = () => {
   mouseDown = false
+}
+
+function randomNess() {
+  const url = ('00' + Math.ceil(Math.random() * 25)).slice(-2)
+  key(url, true)
+}
+
+function randomNotes(bNum) {
+  if (!randomButton[bNum]) {
+    randomButton[bNum] = true
+    randomNess()
+    randomPlay[bNum] = setInterval(randomNess, 1000)
+    document.getElementById(`b${bNum}-light`)?.classList.add('triggered')
+  } else {
+    randomButton[bNum] = false
+    clearInterval(randomPlay[bNum])
+    document.getElementById(`b${bNum}-light`)?.classList.remove('triggered')
+  }
 }
