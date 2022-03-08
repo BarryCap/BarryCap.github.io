76a3d01bf551c91a9f65fd276b8bff0e7ca08994
BarryCap <barthelemy.perez@gmail.com>
2022-03-08 15:41:03 +0100

    Update 25

--- sounds/25/squares.js
+++ sounds/25/squares.js
@@ -1,19 +1,22 @@
 let mouseDown = false
 let randomButton = [false, false]
 let randomPlay = {}
+let soundType = []
 
 async function key(url, press) {
   if (mouseDown || press) {
-    let audio = new Audio(`square/${url}.wav`)
-    document.getElementById(url)?.classList.add('pressed')
-/* random colors */
+    /* random colors */
     const cNum = Math.ceil(Math.random() * 6)
     document.getElementById(url)?.classList.add(`color-${cNum}`)
-/* end of random colors */
-    await audio.play()
-    audio.onended = () => {
-      document.getElementById(url)?.classList.remove('pressed')
-      document.getElementById(url)?.classList.remove(`color-${cNum}`)
+    /* end of random colors */
+    for (sound of soundType) {
+      let audio = new Audio(`${sound}/${url}.wav`)
+      document.getElementById(url)?.classList.add('pressed')
+      await audio.play()
+      audio.onended = () => {
+        document.getElementById(url)?.classList.remove('pressed')
+        document.getElementById(url)?.classList.remove(`color-${cNum}`)
+      }
     }
   }
 }
@@ -36,10 +39,20 @@ function randomNotes(bNum) {
     randomButton[bNum] = true
     randomValue()
     randomPlay[bNum] = setInterval(randomValue, 1000)
-    document.getElementById(`b${bNum}-light`)?.classList.add('triggered')
+    document.getElementById(`r${bNum}-light`)?.classList.add('triggered')
   } else {
     randomButton[bNum] = false
     clearInterval(randomPlay[bNum])
-    document.getElementById(`b${bNum}-light`)?.classList.remove('triggered')
+    document.getElementById(`r${bNum}-light`)?.classList.remove('triggered')
+  }
+}
+
+function changeSound(sNum) {
+  if (!soundType.includes(sNum)) {
+    soundType.push(sNum)
+    document.getElementById(`${sNum}-light`)?.classList.add('triggered')
+  } else {
+    soundType.splice(soundType.indexOf(sNum), 1)
+    document.getElementById(`${sNum}-light`)?.classList.remove('triggered')
   }
 }
